d5ada96bb1963b4f9081d9f3815727a1116adc23
sy518 <samanazehra78692@gmail.com>
2025-05-14 23:57:13 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from 'react'
+import { useState } from 'react'
+import './App.css'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contect from './pages/Contect'
+
+
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+
+
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contect" element={<Contect />} />
+
+        </Routes>
+      </BrowserRouter>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/about.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+
+function About() {
+    const handleClick = () => {
+        alert('Button Clicked!');
+    }
+    return (
+
+        <>
+            <div className="">
+                <nav className="justify-between flex" >
+
+                    <h1 className="text-2xl  text-white uppercase p-2 ">Whispering Woods</h1>
+                    <ul className="flex gap-4 p-2 text-white text-lg uppercase" >
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/about">about</Link>
+                        </li>
+                        <li>
+                            <Link to="/contect">contect</Link>
+                        </li>
+                    </ul>
+
+                    <button id="btn"
+                        className="w-24 bg-black text-white p-1 uppercase rounded-xl hover:skew-6"
+                        onClick={handleClick}
+                    >
+                        click me
+                    </button>
+
+                </nav>
+                <header className="  w-[600px] h-[300px] border-2  border-gray-500 shadow-xl/30 bg-blur rounded-2xl justify-center mx-auto mt-[100px]">
+                    <div className="    text-white">
+                        <h2 className="text-2xl m-4 p-4">The Whispering Woods</h2>
+
+                        <p className="text-md m-4">
+
+                            The Whispering Woods
+                            Emerald canopies reach for the sky, ancient roots embrace the earth below. Life teems within, a symphony of rustling leaves and vibrant calls.
+
+                        </p>
+                        <div>
+
+                            <button
+                                className="w-[150px] bg-black text-white p-1 uppercase rounded-xl hover:skew-6"
+
+                            >
+                                <a href="/contect">
+
+                                    Contect us
+                                </a>
+                            </button>
+                        </div>
+                    </div>
+                </header>
+
+            </div>
+        </>
+    )
+
+}
+
+    export default About
